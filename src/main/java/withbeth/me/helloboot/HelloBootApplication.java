e79560496f427810c95ad2ca6007aee1fa4a9fc1
Bryan | JJ <with.beth@gmail.com>
2023-02-27 00:20:31 +0900

    feat: SpringBootAnnotation쓰지않고, Tomcat ServletContainer띄우기

--- src/main/java/withbeth/me/helloboot/HelloBootApplication.java
+++ src/main/java/withbeth/me/helloboot/HelloBootApplication.java
@@ -2,12 +2,18 @@ package withbeth.me.helloboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class HelloBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloBootApplication.class, args);
+        // SpringApplication.run(HelloBootApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
