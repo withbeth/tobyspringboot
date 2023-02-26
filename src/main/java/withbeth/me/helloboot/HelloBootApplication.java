230f707860b93f353e7757869a8dcc85ef56b0ae
Bryan | JJ <with.beth@gmail.com>
2023-02-27 02:11:10 +0900

    feat: FrontController Servlet추가

--- src/main/java/withbeth/me/helloboot/HelloBootApplication.java
+++ src/main/java/withbeth/me/helloboot/HelloBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -24,20 +25,28 @@ public class HelloBootApplication {
         // SpringApplication.run(HelloBootApplication.class, args);
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext ->
-                servletContext.addServlet("myServlet", new HttpServlet() {
+                servletContext.addServlet("myFrontController", new HttpServlet() {
                     @Override
                     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-                        // Handle Request
-                        // handle "name" param
-                        String name = req.getParameter("name");
+                        // Servlet 공통 로직 구현 skip;
 
-                        // Handle Response
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        // 간단히 body 부분 설정
-                        resp.getWriter().println("hello " + name + " from servlet");
+                        // handler mapping
+                        if (req.getMethod().equals(HttpMethod.GET.name())
+                                && req.getRequestURI().equals("/hello")) {
+                            // Handle Request
+                            // handle "name" param
+                            String name = req.getParameter("name");
+
+                            // Handle Response
+                            resp.setStatus(HttpStatus.OK.value());
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                            // 간단히 body 부분 설정
+                            resp.getWriter().println("hello " + name + " from servlet");
+                        } else {
+                            resp.setStatus(HttpStatus.NOT_FOUND.value());
+                        }
                     }
-                }).addMapping("/hello"));
+                }).addMapping("/*")); // 모든 요청은 front controller로 위임
         webServer.start();
     }
 
