74395dc22b5eb08e2108e4d7325c058e6f2ac399
Bryan | JJ <with.beth@gmail.com>
2023-02-27 00:35:06 +0900

    feat: Servlet등록

--- src/main/java/withbeth/me/helloboot/HelloBootApplication.java
+++ src/main/java/withbeth/me/helloboot/HelloBootApplication.java
@@ -4,7 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 // @SpringBootApplication
 public class HelloBootApplication {
@@ -12,7 +20,16 @@ public class HelloBootApplication {
     public static void main(String[] args) {
         // SpringApplication.run(HelloBootApplication.class, args);
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext ->
+                servletContext.addServlet("myServlet", new HttpServlet() {
+                    @Override
+                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                        resp.setStatus(200);
+                        resp.setHeader("Content-Type", "text/plain");
+                        // 간단히 body 부분 설정
+                        resp.getWriter().println("hello from servlet");
+                    }
+                }).addMapping("/hello"));
         webServer.start();
     }
 
