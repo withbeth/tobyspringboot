8f4657435bedb74a960cf7663fb2433cbb74c88a
Bryan | JJ <with.beth@gmail.com>
2023-03-11 00:28:03 +0900

    feat: Controller안에 Annotation Mapping정보 설정
    - ServletContainer대신, Controller안에 Web Request Mapping정보 설정

--- src/main/java/withbeth/me/helloboot/HelloController.java
+++ src/main/java/withbeth/me/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package withbeth.me.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,6 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         Objects.requireNonNull(name);
         return helloService.sayHello(name);
