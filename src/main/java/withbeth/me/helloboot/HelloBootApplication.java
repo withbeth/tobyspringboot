c8c6cb15a452b062f4bf0df8b7996057bff19f3c
Bryan | JJ <with.beth@gmail.com>
2023-03-11 01:10:02 +0900

    feat: 자바코드 구성정보 이용하여 POJO를 스프링빈으로 등록

--- src/main/java/withbeth/me/helloboot/HelloBootApplication.java
+++ src/main/java/withbeth/me/helloboot/HelloBootApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -22,14 +25,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 // @SpringBootApplication
+@Configuration
 public class HelloBootApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
         // SpringApplication.run(HelloBootApplication.class, args);
 
         // Spring Container 생성
         // DispatcherServlet은 WebAppContext를 인자로 받기에, GenericWebAppContext생성.
-        GenericWebApplicationContext appContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext() {
 
             @Override
             protected void onRefresh() {
@@ -47,10 +61,12 @@ public class HelloBootApplication {
 
             }
         };
+        // 구성정보 메타데이터 설정
+        appContext.register(HelloBootApplication.class);
         // Bean class 지정
-        appContext.registerBean(HelloController.class);
+        // appContext.registerBean(HelloController.class);
         // 스프링이 구성정보를 만들때는, 정확하게 어떤 클래스를 가지고 빈을 만들건지 명시적으로 지정해야 한다.
-        appContext.registerBean(SimpleHelloService.class);
+        // appContext.registerBean(SimpleHelloService.class);
         // 주어진 구성정보 이용해 초기화
         appContext.refresh();
         // 클래스 타입만으로 빈 취득
