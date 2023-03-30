68e80d8a94be7215df0677303b3f47f9b3894157
Bryan | JJ <with.beth@gmail.com>
2023-03-31 00:09:11 +0900

    refactor(HelloController): test용으로 작성했던 AwareInterface삭제

--- src/main/java/withbeth/me/helloboot/HelloController.java
+++ src/main/java/withbeth/me/helloboot/HelloController.java
@@ -1,35 +1,21 @@
 package withbeth.me.helloboot;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import java.util.Objects;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
 @RestController
-public class HelloController implements ApplicationContextAware {
+public class HelloController {
 
     private final HelloService helloService;
-    // AppContext도 스프링 빈으로 등록되어 생성자 주입 받을 수 있다.
-    // AwareInterface쓰는 것도 생성자 주입받는게 인터페이스 구현 필요도 없어지고, final지정도 가능해진다.
-    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
         Objects.requireNonNull(name);
         return helloService.sayHello(name);
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("Set application context before init");
-        System.out.println(applicationContext);
     }
 }
